96e04368f097b0688c954ff739e669526401bbd1
wormingdead <wormingdead@gmail.com>
2024-11-10 19:53:35 -0800

    Fix versions post-v207. Remove support for pre-v207

--- ConversationDelegates.cs
+++ ConversationDelegates.cs
@@ -28,24 +28,12 @@ namespace Mods.Wormingdead.EasyRecruit
     [ConversationDelegate]
     public static void RecruitWormingdeadEasyRecruit(DelegateContext Context)
     {
-      // 207: Spring Molting
-      #if BUILD_2_0_207
-        // Copied from WaterRitualJoinParty.cs (beta:Spring Molting)
-        The.Speaker.SetAlliedLeader<AllyProselytize>(The.Player);
-        Lovesick Effect;
-        if (The.Speaker.TryGetEffect<Lovesick>(out Effect))
-          Effect.PreviousLeader = The.Player;
-        Popup.Show(The.Speaker.Does("join", Stripped: true) + " you!");
-      #else
-        // Copied from WaterRitualJoinParty.cs
-        Brain pBrain = The.Speaker.pBrain;
-        pBrain.BecomeCompanionOf(The.Player);
-        if (pBrain.GetFeeling(The.Player) < 0)
-          pBrain.SetFeeling(The.Player, 0);
-        if (The.Speaker.GetEffect("Lovesick") is Lovesick effect)
-          effect.PreviousLeader = The.Player;
-        Popup.Show(The.Speaker.T() + The.Speaker.GetVerb("join") + " you!");
-      #endif
+      // Copied from WaterRitualJoinParty.cs (beta:Spring Molting)
+      The.Speaker.SetAlliedLeader<AllyProselytize>(The.Player);
+      Lovesick Effect;
+      if (The.Speaker.TryGetEffect<Lovesick>(out Effect))
+        Effect.PreviousLeader = The.Player;
+      Popup.Show(The.Speaker.Does("join", Stripped: true) + " you!");
     }
   }
 }
